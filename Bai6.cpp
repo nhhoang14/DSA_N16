dc699b0bc2a867fff3718910fbfb826f6b978de0
nhhoang.14 <nhhoangne@gmail.com>
2025-02-18 17:10:40 +0700

    Add files via upload

---
+++ Bai6.cpp
@@ -0,0 +1,25 @@
+//SỐ TĂNG GIẢM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int check(){
+  string s;
+  cin >> s;
+  for(int i = 0; i < s.length() - 3; i++){
+    if(s[i] > s[i + 1] && s[i + 1] < s[i + 2])return 0;
+  }
+  return 1;
+}
+
+int main(){
+  int t;
+  cin >> t;
+  cin.ignore();
+  while(t--){
+    if(check()) cout << "YES" << endl;
+    else cout << "NO" << endl;
+  }
+  return 0;
+}
+
