b0e261af6fde6a7aea6a9b92f85b99b1f5e40f7a
nhhoang.14 <nhhoangne@gmail.com>
2025-02-24 00:00:39 +0700

    Update Bai09.cpp

--- Bai09.cpp
+++ Bai09.cpp
@@ -12,7 +12,7 @@ int main(){
     for(char c1 = 'A'; c1 <= 'Z'; ++c1){
         for(char c2 = c1 + 1; c2 <= 'Z'; ++c2){
             if (ve[c1][0] < ve[c2][0] && ve[c2][0] < ve[c1][1] && ve[c1][1] < ve[c2][1]) dem++;
-			if (ve[c2][0] < ve[c1][0] && ve[c1][0] < ve[c2][1] && ve[c2][1] < ve[c1][1]) dem++;
+	    if (ve[c2][0] < ve[c1][0] && ve[c1][0] < ve[c2][1] && ve[c2][1] < ve[c1][1]) dem++;
         }
     }
     cout << dem;
