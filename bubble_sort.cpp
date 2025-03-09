74636f7d4899763d3539f5eb308e751a66612649
nhhoang.14 <nhhoangne@gmail.com>
2025-03-09 13:18:29 +0700

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -9,9 +9,6 @@ void bubble_sort(int a[], int n){
 		for(int j = 0; j < n - i - 1; j++)
 			if(a[j] > a[j + 1])
 				swap(a[j], a[j + 1]);
-				
-	for(int i = 0; i < n; i++)
-		cout << a[i] << " ";
 }
 
 int main(){
@@ -19,5 +16,7 @@ int main(){
 	cin >> n;
 	for(int i = 0; i < n; i++) cin >> a[i];
 	bubble_sort(a, n);
+	for(int i = 0; i < n; i++)
+		cout << a[i] << " ";
 	return 0;
 }
