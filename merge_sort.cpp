422ecee3dd9c06dffe0d1ff74d5549e46bb35a5d
nhhoang.14 <nhhoangne@gmail.com>
2025-03-12 20:21:39 +0700

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -5,39 +5,39 @@ typedef long long ll;
 //O(nlogn)
 // tron 2 day con: day 1:[l, m]; day 2: [m+1, r]
 
-void merge(int a[], int l, int m, int r){
-	vector<int> x(a + l, a + m + 1);
-	vector<int> y(a + m + 1, a + r + 1);
-	int i = 0, j = 0;
-	while(i < x.size() && j < y.size()){
-		if(x[i] < y[j]){
-			a[l] = x[i]; ++l; ++i;	
-		} else {
-			a[l] = y[j]; ++l; ++j;
-		}
-	}
-	while(i < x.size()){
-		a[l] = x[i]; ++l; ++i;
+void merge(vector<int> &res, int l, int m, int r){
+    vector<int> x(res.begin() + l, res.begin() + m + 1);
+    vector<int> y(res.begin() + m + 1, res.begin() + r + 1);
+    int i = 0, j = 0;
+    while (i < x.size() && j < y.size()){
+        if(x[i] < y[j]){
+            res[l++] = x[i++];
+        } else {
+            res[l++] = y[j++];
+        }
+    }
+    while(i < x.size()){
+		res[l++] = x[i++];
 	}
 	while(j < y.size()){
-		a[l] = y[j]; ++l; ++j;
+		res[l++] = y[j++];
 	}
 }
 
-void merge_sort(int a[], int l, int r){
-	if(l >= r) return;
-	int m = (l + r) / 2;
-	merge_sort(a, l, m);
-	merge_sort(a, m + 1, r);
-	merge(a, l, m, r);
+void merge_sort(vector<int> &res, int l, int r){
+    if(l >= r) return;
+    int m = (l + r)/2;
+    merge_sort(res, l, m);
+    merge_sort(res, m + 1, r);
+    merge(res, l, m, r);
 }
 
 int main(){
-	int n = 100;
-	int a[100];
-	for(int i = 0; i < n; i++) cin >> a[i];
-	merge_sort(a, 0, n - 1);
-	for(int x : a)
-		cout << x << " ";
-	return 0;
+	int n;
+        cin >> n;
+        vector<int> a(n);
+        for(auto &x : a) cin >> x;
+        merge_sort(a, 0, n - 1);
+        for(auto x : a) cout << x << " ";
+        return 0;
 }
