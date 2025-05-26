36a9bace601872695a68714b61adcb4201e6a232
nhhoang.14 <nhhoangne@gmail.com>
2025-05-26 23:43:56 +0700

    Create Luỹ_thừa.cpp

---
+++ Luỹ_thừa.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll mod = 1e9 + 7;
+
+ll lt(ll n, ll k){
+    ll res = 1;
+    n %= mod;
+    while (k > 0) {
+        if (k % 2 == 1)
+            res = (res * n) % mod;
+        n = (n * n) % mod;
+        k /= 2;
+    }
+    return res;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        ll n, k;
+        cin >> n >> k;
+        cout << lt(n,k) << endl;
+    }
+    return 0;
+}
