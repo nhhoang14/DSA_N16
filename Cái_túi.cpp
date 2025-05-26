a219758981b29bbfe533d5cfdb8483d4add8d893
nhhoang.14 <nhhoangne@gmail.com>
2025-05-26 23:44:38 +0700

    Update Cái_túi.cpp

--- Cái_túi.cpp
+++ Cái_túi.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void qhd(int a[], int c[], int n, int v){
-	vector<vector<int>> dp(n + 1, vector<int>(v + 1, 0));
-	for(int i = 1; i <= n; i++){
-		for(int j = 0; j <= v; j++){
-			dp[i][j] = dp[i - 1][j]; 
-			if(j >= a[i - 1]){
-				dp[i][j] = max(dp[i][j], dp[i - 1][j - a[i - 1]] + c[i - 1]);
-			}
-		}
-	}
-	cout << dp[n][v] << endl;
-}
+typedef long long ll;
 
 int main(){
-	int t;
-	cin >> t;
-	while(t--){
-		int n, v;
-		cin >> n >> v;
-		int a[n], c[n];
-		for(auto &x : a) cin >> x;
-	    for(auto &x : c) cin >> x;
-		qhd(a, c, n, v);
-	}
-	return 0;
+    int t;
+    cin >> t;
+    while (t--){
+        int n, v;
+        cin >> n >> v;
+        vector<int> w(n);
+        vector<int> val(n);
+        for(auto &x : w) cin >> x;
+        for(auto &x : val) cin >> x;
+        vector<vector<int>> dp(n + 1, vector<int>(v + 1, 0));
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= v; j++){
+                dp[i][j] = dp[i - 1][j];
+                if (j >= w[i - 1]){
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i - 1]] + val[i - 1]);
+                }
+            }
+        }
+        cout << dp[n][v] << endl;
+    }    
+    return 0;
 }
